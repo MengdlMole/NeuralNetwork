010d4ff513efeccc0197fe0ef3b40818357dece5
MengdlMole <mengdl1037@outlook.com>
2023-03-31 11:28:12 +0800

    添加了使用PINN求解反问题的一个代码示例

--- PINN_Inverse/PINN_Inverse.py
+++ PINN_Inverse/PINN_Inverse.py
@@ -10,6 +10,8 @@ import math
 class NeuralNetwork(torch.nn.Module):
     def __init__(self):
         super().__init__()
+        self.gamma = torch.nn.Parameter(torch.rand(
+            1, requires_grad=True))  # the parameter gamma
         self.linear_relu_stack = torch.nn.Sequential(
             torch.nn.Linear(1, 40),
             torch.nn.ReLU(),
@@ -25,9 +27,34 @@ class NeuralNetwork(torch.nn.Module):
             torch.nn.Linear(40, 1)
         )
 
-    def forward(self, x):
-        solution = self.linear_tanh_stack(x)
-        return solution
+    def show_gamma(self):
+        return self.gamma
+
+    # loss function depending on the ODEs
+    def loss_of_eq(self, x, y_pred):
+        pi = math.pi
+        # right_item = torch.from_numpy(right_item0.astype(numpy.float32)).to(device)
+        y_pred_grad = torch.autograd.grad(
+            y_pred.sum(), x, create_graph=True)[0]
+        # TODO: There depends what ODEs you want to solve
+        loss = y_pred_grad - self.gamma * (1 + pi * torch.cos(pi * x / 2) / 2)
+        return loss.t() @ loss
+
+    def loss_of_initial(self, initial_point):
+        initial_exact = exactSolution(initial_point)
+        loss = self.linear_tanh_stack(
+            initial_point.unsqueeze(1)) - initial_exact.unsqueeze(1)
+        return loss.t() @ loss
+
+    def loss_of_solution(self, x, y_pred):
+        y_exact = exactSolution(x)
+        loss = y_pred - y_exact
+        return loss.t() @ loss
+
+    def forward(self, x, initial_point):
+        y_pred = self.linear_tanh_stack(x)
+        return self.loss_of_eq(x, y_pred) + self.loss_of_initial(initial_point) + self.loss_of_solution(x, y_pred)
+        # return self.loss_of_eq(x, y_pred) + self.loss_of_initial(model, initial_point)
 
 
 # define ODEs
@@ -36,36 +63,15 @@ def exactSolution(x):
     return x + torch.sin(pi * x / 2)
 
 
-# loss function depending on the ODEs
-class Loss(torch.nn.Module):
-    def __init__(self):
-        super(Loss, self).__init__()
-
-    def loss_of_eq(self, x, y_pred):
-        pi = math.pi
-        # right_item = torch.from_numpy(right_item0.astype(numpy.float32)).to(device)
-        y_pred_grad = torch.autograd.grad(
-            y_pred.sum(), x, create_graph=True)[0]
-        # TODO: There depends what ODEs you want to solve
-        loss = y_pred_grad - (1 + pi * torch.cos(pi * x / 2) / 2)
-        return loss @ loss
-
-    def loss_of_initial(self, model, initial_point):
-        initial_exact = exactSolution(initial_point)
-        loss = model(initial_point.unsqueeze(1)) - initial_exact.unsqueeze(1)
-        return loss @ loss
-
-    def forward(self, model, x, initial_point, y_pred):
-        return self.loss_of_eq(x, y_pred) + self.loss_of_initial(model, initial_point)
-
-
 # train code
-def train(model, optimizer, loss_fn, x, initial_point):
-    for iter in range(10000):
+def train(device, model, optimizer, loss_fn, x, initial_point):
+    for iter in range(30000):
         # Compute prediction error
-        y_pred = model(x.unsqueeze(1))
+        result = model(x.unsqueeze(1), initial_point)
 
-        loss = loss_fn(model, x, initial_point, y_pred)
+        loss = loss_fn(result, torch.tensor(0, dtype=torch.float32).to(device))
+
+        # loss = loss_fn(model, x, initial_point, y_pred, gamma)
         # loss.requires_grad_()
 
         # Backpropagation
@@ -75,6 +81,7 @@ def train(model, optimizer, loss_fn, x, initial_point):
 
         if iter % 1000 == 0:
             print(f"iter {iter}, Loss: {loss.item()}")
+            print(model.show_gamma())
 
 
 # test code
@@ -85,9 +92,8 @@ def test(model):
 
 def main():
     # Get cpu or gpu device for training
-    device = "cuda" if torch.cuda.is_available(
-    ) else "mps" if torch.backends.mps.is_available() else "cpu"
-    # device = "cpu"
+    # device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+    device = "cpu"
     # device = "cuda"
     print(f"Using {device} device")
 
@@ -100,9 +106,12 @@ def main():
     # model.train()
     model = NeuralNetwork().to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-    loss_fn = Loss()
+    # gamma = model.get_gamma().to(device)
+    loss_fn = torch.nn.MSELoss()
 
-    train(model, optimizer, loss_fn, x, initial_point)
+    train(device, model, optimizer, loss_fn, x, initial_point)
+
+    print(model.show_gamma())
 
 
 if __name__ == '__main__':
